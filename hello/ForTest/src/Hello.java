8d6e97a4df909016d181166655c1898c4d776fc5
user <user@user-PC>
2015-01-14 18:35:29 +0900

    내가했지롱

--- hello/ForTest/src/Hello.java
+++ hello/ForTest/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello JAVA");
-		System.out.println("Hello World!!!!@!!!!@@@@@@해지니가수정한hellojava@@@@@@@!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello World!!!!@!!!!@@@@@@워크스페이스에서 햇지롱 해지니가수정한hellojava@@@@@@@!!!!!!!!!!!!!!!!!!!!!");
 	}
 
 }
