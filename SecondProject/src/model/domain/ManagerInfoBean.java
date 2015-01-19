c732a1298167254df5ae82bf6f001226664a648d
KODB <KODB@192.168.3.73>
2015-01-19 17:05:52 +0900

    managerinfoBean

--- SecondProject/src/model/domain/ManagerInfoBean.java
+++ SecondProject/src/model/domain/ManagerInfoBean.java
@@ -4,18 +4,34 @@ import java.util.Date;
 
 public class ManagerInfoBean extends MemberBean {
 	private int managerInfoNum;
+	private String memberId;
 	private String license;
 	private int managingMemeber;
 	private Date career;
+	private MemberBean member;
 
-	public ManagerInfoBean() {}
+	public ManagerInfoBean() {
+	}
 
-	public ManagerInfoBean(int managerInfoNum, String license, int managingMemeber, Date career) {
+	public ManagerInfoBean(int managerInfoNum, String memberId, String license,
+			int managingMemeber, Date career) {
 		super();
 		this.managerInfoNum = managerInfoNum;
+		this.memberId = memberId;
 		this.license = license;
 		this.managingMemeber = managingMemeber;
 		this.career = career;
+	}
+
+	public ManagerInfoBean(int managerInfoNum, String memberId, String license,
+			int managingMemeber, Date career, MemberBean member) {
+		super();
+		this.managerInfoNum = managerInfoNum;
+		this.memberId = memberId;
+		this.license = license;
+		this.managingMemeber = managingMemeber;
+		this.career = career;
+		this.member = member;
 	}
 
 	public int getManagerInfoNum() {
@@ -50,17 +66,37 @@ public class ManagerInfoBean extends MemberBean {
 		this.career = career;
 	}
 
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public MemberBean getMember() {
+		return member;
+	}
+
+	public void setMember(MemberBean member) {
+		this.member = member;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("ManagerInfoBean [managerInfoNum=");
 		builder.append(managerInfoNum);
+		builder.append(", memberId=");
+		builder.append(memberId);
 		builder.append(", license=");
 		builder.append(license);
 		builder.append(", managingMemeber=");
 		builder.append(managingMemeber);
 		builder.append(", career=");
 		builder.append(career);
+		builder.append(", member=");
+		builder.append(member);
 		builder.append("]");
 		return builder.toString();
 	}
