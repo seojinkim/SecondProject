92ecf03df20c35dfd2efd08d12440aaa44f0a9ce
KODB <KODB@192.168.3.73>
2015-01-19 13:39:53 +0900

    Income.xml과 IncomeDAO 수정

--- SecondProject/src/model/dao/IncomeDAO.java
+++ SecondProject/src/model/dao/IncomeDAO.java
@@ -1,8 +1,8 @@
 package model.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
 import model.domain.SpendBean;
 import model.domain.IncomeBean;
 
@@ -22,4 +22,41 @@ public class IncomeDAO {
 		}
 		return incomeList;
 	}
+
+	public static int insertIncome(IncomeBean income) throws SQLException {
+		SqlSession session = null;
+		int result = 0;
+		try {
+			session = DBUtil.getSqlSession(true);
+			result = session.insert("INCOME.insertIncome", income);
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	public static int updateIncome(IncomeBean income) throws SQLException {
+		SqlSession session = null;
+		int result = 0;
+		try {
+			session = DBUtil.getSqlSession(true);
+			result = session.update("INCOME.updateIncome", income);
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	public static int deleteIncome(String id) throws SQLException {
+		SqlSession session = null;
+		int result = 0;
+		try {
+			session = DBUtil.getSqlSession(true);
+			result = session.delete("INCOME.deleteIncome", id);
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
 }
